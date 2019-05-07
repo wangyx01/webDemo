b5999830a7be8520c42ef408b94fb02558bc24b4
wangyx01 <wyx903023764@163.com>
2019-05-07 16:04:34 +0800

    six commit

--- website/webDemo/settings.py
+++ website/webDemo/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # UPLOAD_DIR = sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 # UPLOAD_DIRS = [os.path.join(UPLOAD_DIR, 'dataprocess')]
